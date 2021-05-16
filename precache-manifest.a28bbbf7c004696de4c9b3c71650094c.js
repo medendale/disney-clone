dbe9d74783830d5182938361fcffd2d127c75216
medendale <70830116+medendale@users.noreply.github.com>
2021-05-15 22:02:08 -0400

    Updates

---
+++ precache-manifest.a28bbbf7c004696de4c9b3c71650094c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d8b3d673f9b09faac25",
+    "url": "/medendale/disney-clone/static/css/main.8d1b4e82.chunk.css"
+  },
+  {
+    "revision": "7d8b3d673f9b09faac25",
+    "url": "/medendale/disney-clone/static/js/main.f85acf73.chunk.js"
+  },
+  {
+    "revision": "2f3796899f096fb4e200",
+    "url": "/medendale/disney-clone/static/js/runtime~main.3432bf6b.js"
+  },
+  {
+    "revision": "249bed44611d33b491c5",
+    "url": "/medendale/disney-clone/static/css/2.1535a707.chunk.css"
+  },
+  {
+    "revision": "249bed44611d33b491c5",
+    "url": "/medendale/disney-clone/static/js/2.f5a94982.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/medendale/disney-clone/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/medendale/disney-clone/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/medendale/disney-clone/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/medendale/disney-clone/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "dc2f8c468478548a7b6f0b9bb4ac93c1",
+    "url": "/medendale/disney-clone/index.html"
+  }
+];
